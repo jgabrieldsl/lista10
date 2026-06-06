fad129b9c7697aefc536a0a4bd07228540533ccd
jgabrieldsl <jgabriel.leite@outlook.com>
2026-06-05 21:08:00 -0300

    feat: implement merge sort

--- main.py
+++ main.py
@@ -9,3 +9,38 @@ def insertion_sort(arr):
             j -= 1
         a[j + 1] = key
     return a
+
+# Algoritmo Merge Sort
+def merge_sort(arr):
+    def _merge(a, left, mid, right):
+        L = a[left : mid + 1]
+        R = a[mid + 1 : right + 1]
+        i = j = 0
+        k = left
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                a[k] = L[i]
+                i += 1
+            else:
+                a[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            a[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            a[k] = R[j]
+            j += 1
+            k += 1
+
+    def _sort(a, lo, hi):
+        if lo < hi:
+            mid = (lo + hi) // 2
+            _sort(a, lo, mid)
+            _sort(a, mid + 1, hi)
+            _merge(a, lo, mid, hi)
+
+    a = arr[:]
+    _sort(a, 0, len(a) - 1)
+    return a
